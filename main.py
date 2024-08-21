ade0693e597dfe1ff945897eaa188a95f66d1f4d
Francina Carron <81628041+francinacarron@users.noreply.github.com>
2024-08-21 12:02:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 # Añadir el directorio raíz al PYTHONPATH
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 # src/main.py
 from api_to_redshift import fetch_data_from_api, process_and_load_data
@@ -10,10 +10,15 @@ from api_to_redshift import fetch_data_from_api, process_and_load_data
 def main():
     """Función principal que orquesta la obtención y carga de datos."""
     data = fetch_data_from_api()
-    if data:
+    print(f"Toma la data {data}")
+    #Chequear que no hay datos duplicados
+    if data.duplicated().any():
+        print("Filtrando datos duplicados...")
+        data = data.drop_duplicates().any()
         process_and_load_data(data)
-    else:
-        print("No se pudo obtener datos de la API.")
+    #Una vez filtrados los duplicados, cargar los datos  ..
+    else:  
+        process_and_load_data(data)
 
 if __name__ == "__main__":
     main()
